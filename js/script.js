195806ca5658cd78ce9681b6cce3074d47ede7e9
Angeloamenta <angeloamenta7@gmail.com>
2021-11-23 15:14:10 +0100

    creazione box

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-
+const container = document.querySelector (".container");
 
 // algoritmo da 1 a 100 con multipli di 3 chiamati fizz, di 5 buzz e di 3 e 5 insieme chiamati fizzbuzz
 
 for (let i = 1; i<=100 ; i++) {
+    const element = document.createElement('div');
+        element.classList.add('box');
+        container.append(element);
 
     if (i % 3 === 0 && i % 5 === 0) {
-
         console.log("FizzBuzz");
     }
 
@@ -18,5 +20,6 @@ for (let i = 1; i<=100 ; i++) {
         console.log("Buzz");
     }else {
         console.log (i)
+        
     }
 }
